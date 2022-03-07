c3cac9f64f6d8c946415639d3c60cdb34bbd3901
kalyani <kalyanichaudhari9420@gmail.com>
2022-03-07 18:01:22 +0530

    'initial'

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+ const expressLayout = require('express-ejs-layouts')
+// now app can use as a object which contain all functionality of express
+const PORT = process.env.PORT || 3300
+
+
+app.get('/' , (req , res) => {
+    res.render('home')
+})
+
+
+// set Template engine
+app.use(expressLayout)
+app.set('views' , path.join(__dirname , '/resources/views'))
+app.set('view engine' , 'ejs')
+
+
+app.listen (PORT , ()=>{
+    console.log(`listen on port ${PORT}`)
+})
