151f9ea2754437d7701b279a57aa846808bf7b32
kalyani <kalyanichaudhari9420@gmail.com>
2022-03-19 00:34:25 +0530

    'authSystem'

--- routes/web.js
+++ routes/web.js
@@ -1,12 +1,18 @@
 const homeController = require("../app/http/controllers/homeController");
 const authController = require("../app/http/controllers/authController");
 const cartController = require("../app/http/controllers/customers/cartController");
+const guest = require('../app/http/middlewares/guest')
 
 function initRoutes(app) {
   //routes - should be after the set template engine
   app.get("/", homeController().index)
-  app.get("/login", authController().login)
-  app.get("/register", authController().register)
+  app.get("/login",guest , authController().login)
+  app.post("/login", authController().postLogin)
+
+  app.get("/register",guest , authController().register)
+  app.post('/register' , authController().postRegister)
+  app.post('/logout' , authController().logout)
+
 
 
   app.get("/cart", cartController().index)
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const connection = mongoose.connection;
-const app = express();
-const ejs = require("ejs");
-const path = require("path");
-const expressLayout = require("express-ejs-layouts");
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const connection = mongoose.connection
+const app = express()
+const ejs = require("ejs")
+const path = require("path")
+const cookieParser = require("cookie-parser");
+const expressLayout = require("express-ejs-layouts")
 // const MongoStore = require('connect-mongo').default;
 // now app can use as a object which contain all functionality of express
 const PORT = process.env.PORT || 3300;
-const session = require("express-session");
-const flash = require("express-flash");
+const session = require("express-session")
+const flash = require("express-flash")
 // const MongoDbStore = require("connect-mongodb-session")(session);
 const MongoDbStore = require("connect-mongo")(session)
+const passport = require("passport")
+
 
 mongoose.connect(
   "mongodb://127.0.0.1/pizza",
@@ -40,45 +43,57 @@ mongoose.connect(
 //     // useCreateIndex: true, useUnifiedTopology: true , useFindAndModify: true
 // });
 
+
 //sessions store
 let mongoStore = new MongoDbStore({
-    url: 'mongodb://127.0.0.1/pizza',
-  mongooseConection: connection,
-  collection: "sessions"
+  url: 'mongodb://127.0.0.1/pizza',
+mongooseConection: connection,
+collection: "sessions"
 });
 
 //session config
 app.use(session({
-    //secret is cooki id which should not declare in code directly declare it in .env file
-    secret: process.env.COOKIE_SECRET,
-    resave: false,
-    store: mongoStore,
-    saveUninitialized: false,
-     cookie: { maxAge: 1000 * 60 * 60 * 24 },  //24 hours
+  //secret is cooki id which should not declare in code directly declare it in .env file
+  secret: process.env.COOKIE_SECRET,
+  resave: false,
+  store: mongoStore,
+  saveUninitialized: false,
+   cookie: { maxAge: 1000 * 60 * 60 * 24 },  //24 hours
 
-  })
+})
 );
+app.use(cookieParser());
+app.use(passport.initialize())
+
+app.use(passport.session())
+//passport config
+const passportInit = require('./app/config/passport')
+passportInit(passport)
+
+
 
 app.use(flash());
 
 //assets
-app.use(express.static("public"));
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended : false}))
 app.use(express.json())
 
 //global middleware
 app.use((req, res , next)=>{
   res.locals.session = req.session
+  res.locals.user = req.user
+
   next()
 
 })
 
 // set Template engine
 app.use(expressLayout);
-app.set("views", path.join(__dirname, "/resources/views"));
-app.set("view engine", "ejs");
-
-require("./routes/web")(app);
+app.set("views", path.join(__dirname, "/resources/views"))
+app.set("view engine", "ejs")
+require("./routes/web")(app)
 
 app.listen(PORT, () => {
-  console.log(`listen on port ${PORT}`);
-});
+  console.log(`listen on port ${PORT}`)
+})
